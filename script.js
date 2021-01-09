02d3c871727b938619c1e689d7cf00dd302ba28e
Ron-Arjay Caluag <arjaycaluag@gmail.com>
2021-01-08 19:14:16 -0800

    pseudocode plan

--- script.js
+++ script.js
@@ -33,12 +33,12 @@ $.ajax({
 var j = 0
 function createRow(area, n) {
 
-    // var arrImg = [
-    //     "https://cdn.cnn.com/cnnnext/dam/assets/210104111236-beginner-gaming-pc-super-169.jpg",
+    var arrImg = [
+        "https://cdn.cnn.com/cnnnext/dam/assets/210104111236-beginner-gaming-pc-super-169.jpg",
 
-    //     "https://media3.s-nbcnews.com/j/newscms/2019_41/3044956/191009-cooking-vegetables-al-1422_ae181a762406ae9dce02dd0d5453d1ba.fit-2000w.jpg"
+        "https://media3.s-nbcnews.com/j/newscms/2019_41/3044956/191009-cooking-vegetables-al-1422_ae181a762406ae9dce02dd0d5453d1ba.fit-2000w.jpg"
 
-    // ]
+    ]
     let rowDiv = $("<div>").addClass("columns")
     for (i = 0; i < n; i++) {
 
@@ -47,7 +47,7 @@ function createRow(area, n) {
         let cardDiv = $("<div>").addClass("card")
         let cardContentDiv = $("<div>").addClass("card-content")
         cardContentDiv.text('asdasdasdasddsad')
-        // let gameImage = $("<img>").attr("src", arrImg[j])
+        cardContentDiv= $("<img>").attr("src", arrImg[j])
         // gameImage.attr("id","id" +j )
         rowDiv.append(colDiv)
         colDiv.append(cardDiv)
@@ -61,5 +61,19 @@ function createRow(area, n) {
     }
 }
 
-createRow($("#description-div"), 2);
-createRow($("#preview-div"), 3);
+
+createRow($("#preview-div"), 2);
+
+
+
+
+
+
+// addEventListner for on('click') [Recipes | Games ]
+// add style.display = 'none'/'block' features - Hides or displays
+// Relocate AJAX functions
+// target specific information that you want to display from APIKey
+// Impliment either checkbox or input field to send criteria call to key
+// 
+
+// 
